de217adcf3a6e6feadbf06b600cc77b599c1ff30
rthelolchex <rthelolchex@gmail.com>
2021-08-23 17:07:09 +0700

    update?

--- plugins/ppcouple.js
+++ plugins/ppcouple.js
@@ -2,7 +2,7 @@ let fetch = require("node-fetch")
 
 let handler = async (m, { conn }) => {
   let res = await fetch(global.API('LeysCoder', '/api/ppcouple', {}, 'apikey'))
-  if (!res.ok) throw await res.text()
+  if (!res.ok) return m.reply(await res.text())
   let json = await res.json()
   if (!json.status) throw json
   await conn.sendFile(m.chat, json.result.male, '', 'cowo', m)
