499b9c58f4f2cceb57002e76c70c389e7601f404
rthelolchex <rthelolchex@gmail.com>
2021-08-30 17:22:53 +0800

    fix igreels

--- plugins/ig_reels.js
+++ plugins/ig_reels.js
@@ -2,19 +2,32 @@ const got = require('got')
 const isUrl = require('is-url')
 const fetch = require('node-fetch')
 const { MessageType } = require("@adiwajshing/baileys")
+const { getInstagramReels } = require('../lib/scrape')
+
 let handler = async (m, { conn, args, text }) => {
   if (!text || isUrl(text) === false) {
         return m.reply("Masukkan url yang valid!")
     }
-    if (text.includes('utm_medium=copy_link')) text = text.replace('utm_medium=copy_link', '__a=1')
-    console.log(text)
-    got(text).then(async (res) => {
-      let data = res.body.split('"video_url":"')[1].split('"')[0]
-      m.reply(`Link ditemukan!\nLink : ${data}`)
-      const resf = await fetch(data, {headers: { 'User-Agent': 'okhttp/4.5.0'}, method: 'GET' })
-      const buff = await resf.buffer()
-      conn.sendMessage(m.chat, buff, MessageType.video, { quoted: m, caption: "Udah bang, nih"})
-    })
+    let reels = await getInstagramReels(text)
+    let res = reels.result
+    let str = `
+Link ditemukan!
+
+Title : ${res.title}
+URL : ${res.url}
+Description : ${res.description}
+Total like : ${res.total_like}
+Total comments : ${res.total_comment}
+Total viewers : ${res.view_count}
+Total diputar : ${res.play_count}
+Link video : ${res.video_link}
+
+_Silahkan tunggu untuk videonya, jika lebih dari 5 menit tidak terkirim, silahkan klik link video untuk mendownload_
+    `.trim()
+    const rbuffer = await fetch(res.video_link, {headers: { 'User-Agent': 'okhttp/4.5.0'}, method: 'GET' })
+    const buff = await rbuffer.buffer()
+    conn.reply(m.chat, str, m)
+    if (buff) conn.sendMessage(m.chat, buff, MessageType.video, { quoted: m, caption: "Udah bang, nih", thumbnail: res.thumbnail })
 }
 handler.help = ['igreels'].map(v => v + ' <url>')
 handler.tags = ['downloader']
