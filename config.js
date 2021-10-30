05f5e7dcc4f709d5b79c673a1cf483bf8016a132
rthelolchex <79995938+rthelolchex@users.noreply.github.com>
2021-10-30 16:19:42 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-global.owner = ['6285161212950', '6282159536790'] // Put your number here
+global.owner = ['6285161212950'] // Put your number here
 global.mods = [] // Want some help?
-global.prems = [] // Premium user has unlimited limit
+global.prems = ["6285710780863", "6281347962643"] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   nrtm: 'https://nurutomo.herokuapp.com',
@@ -25,6 +25,7 @@ global.msg = {
   wait: "_Silahkan tunggu sebentar..._",
   fail: "_Terjadi kesalahan, silahkan coba lagi._"
 }
+global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&fillTextType=1&fillTextPattern=Warning!&text='
 
 // Sticker WM
 global.packname = `Ini weem bang`
